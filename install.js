1871e725aa8d2549dfa0cbc5601f616d7ab8af95
Roland Rodriguez <roland@govcraft.ai>
2024-07-24 21:08:19 -0600

    feat: update npm publish tag and package versioning
    - Changed npm publish tag from alpha to latest in release_and_publish.yml
    - Updated the versioning logic in update_package_json_version.yml to include postinstall script
    - Bumped up paseto_cli version from 1.0.23-alpha.3 to 1.0.24-alpha.3 in Cargo.lock and Cargo.toml files
    - Added console log for binary distribution version in install.js

--- install.js
+++ install.js
@@ -23,6 +23,7 @@ if (process.env.CI) {
 function makeRequest(url) {
     return new Promise((resolve, reject) => {
         https.get(url, (response) => {
+            console.log(`Using binary distribution version: ${BINARY_DISTRIBUTION_VERSION}`);
             console.log(`Attempting to download dependency from ${url}`);
             if (response.statusCode >= 200 && response.statusCode < 300) {
                 const chunks = [];
