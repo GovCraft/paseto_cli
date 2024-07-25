4cf5261c0d53943fd4a6e4958e3da87f9d88f860
Roland Rodriguez <roland@govcraft.ai>
2024-07-25 03:16:02 -0600

    feat: add platform-specific binary distribution packages and update version to 1.0.0-alpha.17
    This commit introduces a lookup table for platform-specific binary distribution packages in the CLI, along with a function to determine the correct package name based on the current platform. It also handles special cases for Windows binaries ending with .exe.
    
    In addition, it updates the paseto_cli version from 1.0.0-alpha.16 to 1.0.0-alpha.17 in both Cargo.lock and Cargo.toml files.
    
    A new index.js file is added which exports a runBinary function that executes the appropriate binary file using childProcess.execFileSync method.

---
+++ index.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const childProcess = require("child_process");
+
+// Lookup table for all platforms and binary distribution packages
+const BINARY_DISTRIBUTION_PACKAGES = {
+    'linux-x64': 'paseto_cli-linux-x64',
+    'darwin-x64': 'paseto_cli-darwin-x64',
+    'darwin-aarch64': 'paseto_cli-darwin-aarch64',
+    'win32-x64': 'paseto_cli-win32-x64'
+};
+
+// Windows binaries end with .exe so we need to special case them.
+const binaryName = process.platform === "win32" ? "paseto_cli.exe" : "paseto_cli";
+
+// Determine package name for this platform
+const platformSpecificPackageName =
+    BINARY_DISTRIBUTION_PACKAGES[process.platform];
+
+function getBinaryPath() {
+    try {
+        // Resolving will fail if the optionalDependency was not installed
+        return require.resolve(`${platformSpecificPackageName}/bin/${binaryName}`);
+    } catch (e) {
+        return path.join(__dirname, "..", binaryName);
+    }
+}
+
+module.exports.runBinary = function (...args) {
+    childProcess.execFileSync(getBinaryPath(), args, {
+        stdio: "inherit",
+    });
+};
